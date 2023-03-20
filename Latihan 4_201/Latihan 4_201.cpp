794de0dabcdb842a2f3e0ca3de639693e25b0b50
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2023-03-20 14:50:30 +0700

    Output Pilihan Menu

--- Latihan 4_201/Latihan 4_201.cpp
+++ Latihan 4_201/Latihan 4_201.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+using namespace std;
 
+float LuasPersegiPanjang(float p, float l) {
+	return p * l;
+}
+float LuasLingkaran(float r) {
+	return 3.14 * r * r;
+}
+float LuasSegitiga(float a, float t) {
+	return 0.5 * a * t;
+}
+
+int main()
+{
+	float panjang, lebar, alas, jejari, tinggi;
+	int pilihan;
+
+	do {
+		cout << "\n\n====================" << endl;
+		cout << "========MENU========" << endl;
+		cout << "====================" << endl;
+		cout << "1. Luas Persegi Panjang" << endl;
+		cout << "2. Luas Lingkaran" << endl;
+		cout << "3. Luas Segitiga" << endl;
+		cout << "Exit" << endl;
+		cin >> pilihan;
+	}
+}
